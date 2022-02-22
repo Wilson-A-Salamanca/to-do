67120e3d72fee8ba052ed2b2c233c57b78a86093
wilson salamanca <wasgonzalez@outlook.es>
2022-02-21 23:08:37 -0500

    delete functionality

--- app.js
+++ app.js
@@ -9,36 +9,29 @@ let task;
 newTask.addEventListener("change", e =>{
   task = e.target.value;
 });
-
+let cont = 0;
 addTask.addEventListener("click", () => {
-  //checkbox for check the task
+  //checkbox for check the task and revome button
   let taskCheck = "";
-  taskCheck += `<input type="checkbox" class="task__check">`;
-  //create task, add the text and a class
+  taskCheck += `<input type="checkbox" class="task__check">
+  <button class="task__add--btn" id="remove__task">x</button>`;
+  //create task, add the text, a class and revome button
   let taskContent = document.createElement("li");
   taskContent.classList.add ("task__text");
+  taskContent.setAttribute("id", cont++);//contador
   taskContent.innerHTML = task + taskCheck;
-
-  //create a button for delete the task with a class
-  let deleteTask = document.createElement("button");
-  deleteTask.classList.add ("task__add--btn");
-  deleteTask.textContent = deleteTask.textContent + "x";
-
-  //add the tasks and delete button
+  //add the tasks
   listTask.appendChild(taskContent);
-  taskContent.appendChild(deleteTask);
-
-  //event for delete button(s)
-  deleteTask.addEventListener("click", () =>{
-    listTask.removeChild(taskContent);
-  })
 });
 
+listTask.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON"){
+    document.getElementById(e.target.parentNode.id).remove();
+  }
 
-
-
-
-
-
-
-
+  if (e.target.nodeName === "INPUT"){
+    //e.target.parentNode.textContent.setAttribute("class", "chikis")
+    console.log(e.target.parentNode.textContent)
+  }
+  e.preventDefault();
+})
